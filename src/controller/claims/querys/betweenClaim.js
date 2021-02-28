7f1e20955e0d79a30a6a703ed09c2b8ad98beb3e
Matata <matias.agf@gmail.com>
2021-02-28 20:46:54 -0300

    SOLUCIONADO problema con la consulta between, se muestrabn los nuevos primeros por fecha y luego en orden de status

--- src/controller/claims/querys/betweenClaim.js
+++ src/controller/claims/querys/betweenClaim.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const { Op } = require('sequelize');
+const sequelize = require('../../../database/conect');
 const router = express.Router();
 const  { Claim, Employee } = require('../../../database/tables');
-const { createDate } = require('../../../services/time')
+const { createDate } = require('../../../services/time');
+const { QueryTypes } = require('sequelize');
 
 module.exports = router.get('/', async (req, res) => {
     try {
@@ -21,13 +23,21 @@ module.exports = router.get('/', async (req, res) => {
                 }
             });
 
+            
+
             claims = await Claim.findAll({
                 where: {
                     EmployeeId: employee.dataValues._id,
                     dayofclaim: {
                         [Op.between]: [startDate, endDate]
                     }
-                }
+                },
+
+                order: [
+                    [sequelize.fn('FIELD', sequelize.col('status'), 'Nuevo', 'Leído', 'Contestado', 'Resuelto')],
+                    ['dayofclaim', 'ASC']
+                ],
+
             })
         
             if(claims.length === 0) {
@@ -36,22 +46,23 @@ module.exports = router.get('/', async (req, res) => {
                 })
             }
         } else {
+            
+            return res.status(404).send({Message: 'No se encontraron reclamos en el periodo seleccionado'})
         
-            claims = await Claim.findAll({
-                where: {
-                    dayofclaim: {
-                        [Op.between]: [startDate, endDate]
-                    }
-                },
-
-                order: [['dayofclaim', 'ASC']],
-            })
+            // claims = await sequelize.query(`
+            //     SELECT '_id', 'type', 'dayofclaim', 'content', 'tracing', 'status', 'linkemail', 'EmployeeId' 
+            //     FROM 'hrdatabase'.'claims' AS 'Claim'
+            //     WHERE 'Claim'.'EmployeeId' = 4 
+            //     AND 'Claim'.'dayofclaim' BETWEEN '2021-03-10' AND '2021-03-13'
+            //     ORDER BY FIELD('status', 'Nuevo', 'Leído', 'Contestado', 'Resuelto')`, 
+            //     {
+            //         type: QueryTypes.SELECT,
+            //         nest: true
+                
+            //     }
+            // )
         
-            if(claims.length === 0) {
-                return res.send({
-                    Message: 'No existen resultados'
-                })
-            }
+            
         }
 
         return res.status(200).send({Query: claims})
